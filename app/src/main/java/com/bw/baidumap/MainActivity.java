56fae254a0dd3f94593c3a27621ef3935bad9882
wu710566132 <710566132@qq.com>
2017-06-27 10:47:28 +0800

    Initial commit

---
+++ app/src/main/java/com/bw/baidumap/MainActivity.java
@@ -0,0 +1,84 @@
+package com.bw.baidumap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+
+public class MainActivity extends Activity {
+
+    MapView mMapView;
+    private BaiduMap mBaiduMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        //普通地图
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+
+
+        //我的位置
+        // 116.305859,40.046536
+        //Location location=new Location(40.046535, 116.30586);
+        //定位
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        // 构造定位数据
+        MyLocationData locData = new MyLocationData.Builder()
+                //  .accuracy(location.getRadius())
+                // 此处设置开发者获取到的方向信息，顺时针0-360
+                .direction(100).latitude(40.046536f)
+                .longitude(116.305859f).build();
+        // 设置定位数据
+        mBaiduMap.setMyLocationData(locData);
+        // 设置定位图层的配置（定位模式，是否允许方向信息，用户自定义定位图标）
+        BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory
+                .fromResource(R.mipmap.ic_launcher);
+
+        MyLocationConfiguration.LocationMode mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;
+        MyLocationConfiguration config = new MyLocationConfiguration(mCurrentMode, true, mCurrentMarker);
+        mBaiduMap.setMyLocationConfiguration(config);
+         /*//当不需要定位图层时关闭定位图层
+          mBaiduMap.setMyLocationEnabled(false);
+*/
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+}
